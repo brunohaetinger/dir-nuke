1dfd52b8875aac3aafbbeaf14f46061da52d870e
Bruno Haetinger <brunohaetinger@gmail.com>
2025-07-16 07:13:29 -0300

    Rename build script to package

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,8 +1,10 @@
 version=$(grep ^version Cargo.toml | head -n1 | cut -d'"' -f2)
+arch=$(uname -m)
+kernel=$(uname -s)
 mkdir -p dist
 cp target/release/dir-nuke dist/dir-nuke
 
 cd dist
-tar -czf dir-nuke-v${version}-x86_64-unknown-linux-gnu.tar.gz dir-nuke
+tar -czf dir-nuke-v${version}-${arch}-${kernel}.tar.gz dir-nuke
 cd ..
 
