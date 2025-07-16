12ae6c85de0a50d5f52b0012ba4a28ba29cd4e67
Bruno Haetinger <brunohaetinger@gmail.com>
2025-07-15 23:36:03 -0300

    Add script to create mocked directories to delete

---
+++ scripts/mock-dirs.sh
@@ -0,0 +1,4 @@
+mkdir -p fake_modules2/node_modules/test/node_modules
+mkdir -p fake_modules3/node_modules/test/node_modules
+mkdir -p fake_modules4/node_modules/test/node_modules
+mkdir -p fake_modules5/node_modules/test/node_modules
