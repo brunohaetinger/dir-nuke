7f4fc480b3300d38bcccc5ce825e007fad5c6a82
Bruno Haetinger <brunohaetinger@gmail.com>
2025-07-22 23:05:28 -0300

    TUI: utility to center a widget

--- src/bin/tui.rs
+++ src/bin/tui.rs
@@ -261,6 +261,27 @@ fn human_label(entry: &NodeModuleEntry) -> String {
     format!("{} - {}", size, entry.path.display())
 }
 
+// Utility to center a widget
+fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - percent_y) / 2),
+            Constraint::Percentage(percent_y),
+            Constraint::Percentage((100 - percent_y) / 2),
+        ])
+        .split(r);
+
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - percent_x) / 2),
+            Constraint::Percentage(percent_x),
+            Constraint::Percentage((100 - percent_x) / 2),
+        ])
+        .split(popup_layout[1])[1]
+}
+
 fn main() -> io::Result<()>{
     if is_help() {
         println!("dir-nuke is a safe and fast CLI tool to delete or \"nuke\" directories.\n        Usage: dir-nuke <search_path>        \n");
